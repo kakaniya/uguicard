c2ea9924c3f43ba0a5f78049e41fbbb523287bbd
kakaniya <myflasky@163.com>
2017-07-14 12:50:30 +0800

    no message

---
+++ scripts/cardsmove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class cardsmove : MonoBehaviour,IBeginDragHandler,IDragHandler,IEndDragHandler {
+
+
+	public void OnBeginDrag(PointerEventData eventDate)
+	{
+		Debug.Log ("OnBeginDrag");
+	}
+
+	public void OnDrag(PointerEventData eventData)
+	{
+		Debug.Log ("OnDrag");
+
+		this.transform.position = eventData.position;
+	}
+
+	public void OnEndDrag(PointerEventData eventData)
+	{
+		Debug.Log ("OnEndDrag");
+	}
+}
